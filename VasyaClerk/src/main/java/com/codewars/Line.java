7d57532c752288e80215e7442b5539a519fcdc97
ucarlosgz <carlosgomez@seti.com.co>
2017-07-18 00:21:09 -0500

    Vasya Clerk
    Final solution kata

--- VasyaClerk/src/main/java/com/codewars/Line.java
+++ VasyaClerk/src/main/java/com/codewars/Line.java
@@ -1,18 +1,25 @@
 package com.codewars;
 
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 public class Line {
 
-    public static String tickets(int[] peopleInLine) {
-        if (peopleInLine.length == 0) return "NO";
+  public static String Tickets(int[] peopleInLine) {
+    if (peopleInLine == null || peopleInLine.length == 0) return "NO";
+    int totalInTickets = 0;
 
-        int totalAmountInQueue = 0;
-        int totalIntickets = 0;
-
-        for (int i = 0; i < peopleInLine.length; i++) {
-            if (totalIntickets - (peopleInLine[i] - 25) < 0) return "NO";
-            totalIntickets += 25;
-        }
-
-        return "YES";
+    for (int i = 0; i < peopleInLine.length; i++) {
+      if (peopleInLine[i] == 25) {
+        totalInTickets += 25;
+        continue;
+      }
+      totalInTickets = totalInTickets - (peopleInLine[i] - 25);
+      if (totalInTickets <= 0 ) return "NO";
+      totalInTickets +=25;
     }
+    return "YES";
+  }
+
+
 }
--- VasyaClerk/src/test/java/com/codewars/LineTest.java
+++ VasyaClerk/src/test/java/com/codewars/LineTest.java
@@ -7,8 +7,7 @@ import static junit.framework.TestCase.assertEquals;
 public class LineTest {
 
     @Test
-    public void test1() {
-        assertEquals("YES", Line.tickets(new int[] {25, 25, 50}));
+    public void test1() {assertEquals("YES", Line.tickets(new int[] {25, 25, 50}));
     }
 
     @Test
@@ -18,6 +17,6 @@ public class LineTest {
 
     @Test
     public void test3() {
-        assertEquals("NO", Line.tickets(new int []{50}));
+        assertEquals("NO", Line.tickets(new int []{25, 25, 25, 25, 25, 100, 100}));
     }
 }
